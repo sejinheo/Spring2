31b07f15be70f2cb99e81f72a77ac9d0f7c70c60
허세진 <heosejin222@gmail.com>
2025-02-10 18:07:44 +0900

    옵션 처리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -41,6 +41,7 @@ public class AppConfig {
     public OrderService orderService(){
         System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
+
     }
     @Bean
     public DiscountPolicy discountPolicy(){
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,8 +13,9 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+   private final MemberRepository memberRepository;
+   private final DiscountPolicy discountPolicy;
+
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
